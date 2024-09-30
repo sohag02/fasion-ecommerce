f8b50100f77b1fbe978bd070f5536e5aec1cec58
Sohag Jabed <sohagjabed10@gmail.com>
2024-09-30 20:33:35 +0530

    bug fixes

--- src/components/category-row.tsx
+++ src/components/category-row.tsx
@@ -1,28 +1,14 @@
 import Link from "next/link";
 import { client } from "@/sanity/lib/client";
 
-// const categories = [
-//   { name: "New Arrivals", href: "/new-arrivals" },
-//   { name: "Men", href: "/men" },
-//   { name: "Women", href: "/women" },
-//   { name: "Kids", href: "/kids" },
-//   { name: "Accessories", href: "/accessories" },
-// ];
-interface Category {
-  title: string;
-  slug: {
-    current: string;
-  };
-}
-
 export async function CategoryRow() {
-	const categories: Category[] = await client.fetch(`*[_type == "category"]{title, slug}`)
+	const categories = await client.fetch(`*[_type == "category"]{title, slug}`)
   return (
     <div>
       {categories.map((category) => (
         <Link
           key={category.title}
-          href={category.slug.current}
+          href={category.slug!.current!}
           className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
         >
           {category.title}
