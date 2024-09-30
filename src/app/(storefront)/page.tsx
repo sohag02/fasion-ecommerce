e8b3fc5e7f2abefc1007d85455b61c86a09e0ef4
Sohag Jabed <sohagjabed10@gmail.com>
2024-09-30 12:10:10 +0530

    separated admin and storefront

--- src/app/(storefront)/page.tsx
+++ src/app/(storefront)/page.tsx
@@ -1,11 +1,9 @@
-import { Navbar } from '@/components/navbar'
 import { HeroSlider } from '@/components/components-hero-slider'
 import { ProductCard } from '@/components/product-card'
 
 export default function Home() {
   return (
     <div className="bg-white">
-      <Navbar />
       <HeroSlider />
       <div className='grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 m-2 px-2 py-4'>
         <ProductCard image="/placeholder.svg" name="Product Name" price={100} slug="product-slug" />
