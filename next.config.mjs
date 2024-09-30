9d4ed8098a96c7b74051967b5b9d202f61e150d4
Sohag Jabed <sohagjabed10@gmail.com>
2024-09-30 11:15:00 +0530

    allowed sanity cdn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'cdn.sanity.io',
+                protocol: 'https',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
